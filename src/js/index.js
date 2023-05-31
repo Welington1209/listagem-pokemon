3c0a0783021e5f47b6f1fa07f5d23bf5f8f0c120
Welington1209 <133167813+Welington1209@users.noreply.github.com>
2023-05-31 14:43:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const imgagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", ()=> {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+  
+   body.classList.toggle("modo-escuro");
+   
+   header.classList.toggle("modo-escuro");
+   
+   footer.classList.toggle("modo-escuro");
+   
+   if(modoEscuroEstaAtivo){
+    
+     imgagemBotaoTrocaDeTema.setAttribute("src", "../src/imagens/sun.png");
+   }
+   else{
+    
+    imgagemBotaoTrocaDeTema.setAttribute("src", "../src/imagens/moon.png");
+   }
+  
+});
